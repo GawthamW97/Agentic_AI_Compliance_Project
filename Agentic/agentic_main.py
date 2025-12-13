54716f2a25895a561e7c71559f0f34ab78e909d1
GawthamVS <pgawtham@varstreet.com>
2025-12-13 15:22:34 +0000

    Final Submission changes

--- Agentic/agentic_main.py
+++ Agentic/agentic_main.py
@@ -1,12 +1,15 @@
-from agentic_evaluation_pipeline import evaluate_agentic_pipeline
+from agentic_evaluation_pipeline import evaluate_agentic_pipeline,evaluate_agentic_hierarchy
 
 #------------------ DATATSET 02 ------------------
 print("DATATSET 02 results")
 # #Flat RAG + Agentic AI
 evaluate_agentic_pipeline("./dataset/test_ext_hs_codes.csv",top_k=20)
 
+# evaluate_agentic_hierarchy("./dataset/test_ext_hs_codes.csv",top_k=20)
 
 #------------------ Synthetic DATASET ------------------
 print("Synthetic Dataset results")
 # #Flat RAG + Agentic AI
 evaluate_agentic_pipeline("./dataset/synthetic_product_descriptions.csv",top_k=20)
+
+# evaluate_agentic_hierarchy("./dataset/synthetic_product_descriptions.csv",top_k=20)
